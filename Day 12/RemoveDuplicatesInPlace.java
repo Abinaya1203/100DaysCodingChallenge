3ae25ffbd3864505bd779daaa963f0ff318f7a45
ABINAYA S <94049206+Abinaya1203@users.noreply.github.com>
2025-08-16 09:27:59 +0530

    Update RemoveDuplicatesInPlace.java

--- Day 12/RemoveDuplicatesInPlace.java
+++ Day 12/RemoveDuplicatesInPlace.java
@@ -1,25 +1,18 @@
 package com.leetcode;
 
-public class RemoveDuplicatesInPlace {
-    public static void main(String[] args) {
-        String str = "programming";
-        char[] chars = str.toCharArray();
-        int writeIndex = 0; 
-        for (int i = 0; i < chars.length; i++) {
-            boolean isDuplicate = false;
-            for (int j = 0; j < writeIndex; j++) {
-                if (chars[i] == chars[j]) {
-                    isDuplicate = true;
-                    break;
-                }
-            }
-            if (!isDuplicate) {
-                chars[writeIndex] = chars[i];
-                writeIndex++;
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        if (n <= 2) 
+        return n;
+        int i = 2; 
+        for (int j = 2; j < n; j++) {
+            if (nums[j] != nums[i - 2]) {
+                nums[i] = nums[j];
+                i++;
             }
         }
-        String result = new String(chars, 0, writeIndex);
-        System.out.println("Original: " + str);
-        System.out.println("Without Duplicates: " + result);
+
+        return i; 
     }
 }
