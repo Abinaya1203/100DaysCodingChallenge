f8baf8877c144d1882cf575071bbaf0f3ee8b460
ABINAYA S <94049206+Abinaya1203@users.noreply.github.com>
2025-08-17 20:54:47 +0530

    Update ValidParenthesisRecursion.java

--- Day 14/ValidParenthesisRecursion.java
+++ Day 14/ValidParenthesisRecursion.java
@@ -1,1 +1,10 @@
+public class ValidParenthesesRecursion {
+    public static boolean isValid(String s) {
+        if (s.isEmpty()) return true;
+        String updated = s.replace("()", "")
+                          .replace("{}", "")
+                          .replace("[]", "");
+        if (updated.equals(s)) return false;
+        return isValid(updated);
+    }
 
